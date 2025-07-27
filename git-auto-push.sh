68401fbe6f5d9c5e1dd6169dbf7674d47c33c8f4
BekirLabjani <business.life.bekir@gmail.com>
2025-07-27 23:24:24 +0200

    Update 0.1

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -1,8 +1,16 @@
 @echo off
 cd /d G:\develper\angular-projects\webside-from-taxidriver
 
+echo 🔧 Starte Angular Build...
+ng build --configuration=production
+
+echo 🟢 Build abgeschlossen. Git-Vorgang beginnt...
+
 git remote set-url origin git@github.com:BekirLabjani/go-taxi-web-page.git
 
 git add .
 git commit -m "Update 0.1"
 git push origin main
+
+echo ✅ Fertig!
+pause
