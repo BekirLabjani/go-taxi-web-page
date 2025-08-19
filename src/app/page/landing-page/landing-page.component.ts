b1075c94b97d4670c6e480abc8917fb9c4cdf5a7
BekirLabjani <business.life.bekir@gmail.com>
2025-08-19 14:49:00 +0200

    Update 0.1

--- src/app/page/landing-page/landing-page.component.ts
+++ src/app/page/landing-page/landing-page.component.ts
@@ -3,11 +3,13 @@ import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { MainComponent } from "../main/main.component";
 import { BookingComponent } from "../booking/booking.component";
 import { CarCardsComponent } from '../car-cards/car-cards.component';
+import { AdvantagesComponent } from "../advantages/advantages.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavbarComponent, MainComponent, BookingComponent, CarCardsComponent],
+  imports: [NavbarComponent, MainComponent, BookingComponent, CarCardsComponent, AdvantagesComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
